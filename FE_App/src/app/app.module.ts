cb3ae2cadcb79cd5a2cce79a9680277d69c9632d
Joy Evans <joyevans@me.com>
2022-02-06 17:52:52 -0600

    Modified routers

--- FE_App/src/app/app.module.ts
+++ FE_App/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'; //added SPA Routing Code
 import { AppComponent } from './app.component';
 import { AnswerComponent } from './answer/answer.component';
 import { BingoCardComponent } from './bingo-card/bingo-card.component';
@@ -11,6 +12,7 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { PlayerComponent } from './player/player.component';
 import { SeriesComponent } from './series/series.component';
 import { UserComponent } from './user/user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'; //added SPA Routing Code
 
 @NgModule({
   declarations: [
@@ -22,11 +24,20 @@ import { UserComponent } from './user/user.component';
     LeaderboardComponent,
     PlayerComponent,
     SeriesComponent,
-    UserComponent
+    UserComponent,
+    PageNotFoundComponent //added SPA Routing Code
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    // //added SPA Routing Code
+    // RouterModule.forRoot([
+    //   {path: 'game', component: GameComponent},
+    //   {path: 'leaderboard', component: LeaderboardComponent},
+    //   {path: '', redirectTo: '/game', pathMatch: 'full'},
+    //   {path: '**', component: PageNotFoundComponent}
+    // ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
