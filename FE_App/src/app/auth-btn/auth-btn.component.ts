f8af03b2b36be2c429d2fef229ec73c5c7bba189
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-06 14:01:26 -0600

    first fixes

--- FE_App/src/app/auth-btn/auth-btn.component.ts
+++ FE_App/src/app/auth-btn/auth-btn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-auth-btn',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthBtnComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
+  logIn(){
+    this.auth.loginWithRedirect();
+  }
+
+  logOut(){
+    this.auth.loginWithRedirect();
+  }
+  loggedIn: boolean = false;
   ngOnInit(): void {
+    this.auth.isAuthenticated$.subscribe((isLoggedIn) =>{
+      this.loggedIn = isLoggedIn;
+    })
   }
 
 }
