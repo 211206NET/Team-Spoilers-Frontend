e19b28d5d2c64a940f85cb301c982de3016b7101
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-07 14:56:45 -0600

    Dealing with some errors

--- FE_App/src/app/auth-btn/auth-btn.component.ts
+++ FE_App/src/app/auth-btn/auth-btn.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-auth-btn',
   templateUrl: './auth-btn.component.html',
   styleUrls: ['./auth-btn.component.css']
+})
+@Injectable({
+  providedIn: 'root'
 })
 export class AuthBtnComponent implements OnInit {
 
