f37fbaa5a8932c585a8bc5a6adb9eb6820af41da
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-07 20:40:55 -0600

    Additions to the navbar

--- FE_App/src/app/navbar/navbar.component.spec.ts
+++ FE_App/src/app/navbar/navbar.component.spec.ts
@@ -6,4 +6,26 @@ import { NavbarComponent } from './navbar.component';
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
-})
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarComponent],
+      imports: [
+        RouterTestingModule,
+        AuthModule.forRoot({
+          domain: 'testing domain',
+          clientId: 'testing clientId'
+        }),
+      ]
+    })
+    .compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
