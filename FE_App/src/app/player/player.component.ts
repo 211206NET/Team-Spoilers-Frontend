7e24a003d1056f1d15b0b033aa1ae7fc8e3f5b70
mcoffey4999 <55113200+mcoffey4999@users.noreply.github.com>
2022-02-09 16:26:09 -0600

    Update player.component.ts

--- FE_App/src/app/player/player.component.ts
+++ FE_App/src/app/player/player.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@auth0/auth0-angular';
+import { BGApiService } from '../services/bgapi.service';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: BGApiService, private auth: AuthService) { }
+
+  users: User[] = [];
+  isLoggedIn: boolean = false;
 
   ngOnInit(): void {
+    this.apiService.getUser().then(result =>{
+      this.users = result;
+    });
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) =>{
+      this.isLoggedIn = isAuthenticated;
+    })
+
   }
 
 }
